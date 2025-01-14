1a568c0aa7be7d06e1121ca2a380c92795150a6b
AdmiralKeso <cas.hagenaar@gmail.com>
2025-01-14 12:45:18 +0100

    Add: On click to run jump function for character jumping

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var jump = document.getElementById("jump-btn");
 var score = document.getElementById("score");
+
+jump.addEventListener("click", function() {
+    character.classList.add("animate"); //Add the jump animation
+    character.addEventListener("animationend", function () { //Remove jump animation
+          character.classList.remove("animate");
+        },{ once: true }); //Ensures that it only runs once
+});
