4787ac4ec50f54894f7fd3738ed180bfcde95c15
AdmiralKeso <cas.hagenaar@gmail.com>
2025-01-14 12:44:14 +0100

    Add: Declaring variables

---
+++ script.js
@@ -0,0 +1,4 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var jump = document.getElementById("jump-btn");
+var score = document.getElementById("score");
