f65259fe90c13d3c0ae3a91b129dd4c371d55b10
AdmiralKeso <cas.hagenaar@gmail.com>
2025-01-15 19:14:08 +0100

    Update: Jump function when active isJumping = true

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ var block = document.getElementById("block");
 var jump = document.getElementById("jump-btn");
 var scoreDisplay = document.getElementById("score");
 var score = 0;
+isJumping = false;
 
 jump.addEventListener("click", function() {
+  isJumping = true;
     character.classList.add("animate"); //Add the jump animation
+
     character.addEventListener("animationend", function () { //Remove jump animation
           character.classList.remove("animate");
+          isJumping = false;
         },{ once: true }); //Ensures that it only runs once
 });
